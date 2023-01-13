7b9648250b4986fa013fa665acf4d1a9121ce5de
LuisHernandezCoding <luishernandezcoding@gmail.com>
2023-01-13 17:25:47 -0600

    Added Webpack

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import './style.css';
+import Icon from './icon.png';
+
+function component() {
+  const element = document.createElement('div');
+
+  element.innerHTML = 'Hello webpack';
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  return element;
+}
+
+document.body.appendChild(component());
