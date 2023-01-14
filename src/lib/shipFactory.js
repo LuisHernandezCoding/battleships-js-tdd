5b16233f8234f584cafa9806920fb272bac4f972
LuisHernandezCoding <luishernandezcoding@gmail.com>
2023-01-13 18:23:56 -0600

    Added Ship Factory function an tests

---
+++ src/lib/shipFactory.js
@@ -0,0 +1,15 @@
+function shipFactory(length) {
+  const ship = {
+    length,
+    hits: [],
+    hit(position) {
+      this.hits.push(position);
+    },
+    isSunk() {
+      return this.hits.length === this.length;
+    },
+  };
+  return ship;
+}
+
+module.exports = shipFactory;
